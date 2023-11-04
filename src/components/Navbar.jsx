0df186456afd78a197e6b422aae85488aafef937
chethanavirajini <cheth.virajini@gmail.com>
2023-11-04 19:55:32 +0530

    customized app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Page1 from "../pages/Page1"
+
+
+export default function Navbar(){
+    return(
+        <>
+            <div style={{padding:'30px', backgroundColor:'aqua'}}>
+            
+            </div>
+
+        </>
+    )
+}
